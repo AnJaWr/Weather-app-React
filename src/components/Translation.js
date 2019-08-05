68179ec1938d61d5a403f3ed7450447a16613e5c
AnJaWr <october3@o2.pl>
2019-08-05 19:14:58 +0200

    Translation in outer file

--- src/components/Translation.js
+++ src/components/Translation.js
@@ -1,10 +1,10 @@
 var translation = [
-      ["light rain", "lekki deszcz"],
-      ["few clouds", "niewielkie zachmurzenie"],
-      ["scattered clouds", "rozproszone chmury"],
-      ["broken clouds", "przejaśnienia"],
-      ["shower rain", "lekki deszcz"],
-      ["rain", "deszcz"],
+   ["light rain", "lekki deszcz"],
+   ["few clouds", "niewielkie zachmurzenie"],
+   ["scattered clouds", "rozproszone chmury"],
+   ["broken clouds", "przejaśnienia"],
+   ["shower rain", "lekki deszcz"],
+   ["rain", "deszcz"],
    ["thunderstorm", "burza"],
    ["thunderstorm with rain", "burza i deszcz"],
    ["thunderstorm with heavy rain", "burza i deszcz"],
@@ -17,159 +17,47 @@ var translation = [
    ["heavy intensity drizzle", "intensywna mżawka"],
    ["light intensity drizzle rain", "mżawka, deszcz, natężenie światła"],
    ["drizzle rain", "deszcz, mżawka"],
-["heavy intensity drizzle rain", "intensywna mżawka"],
-["shower rain and drizzle", "deszcz"],
-["heavy shower rain and drizzle", "ulewny deszcz i mżawka"],
-["shower drizzle", "mżawka"],
-["drizzle", "mżawka"],
-["light rain", "lekki deszcz"],
-["moderate rain", "umiarkowane opady"],
-["heavy intensity rain", "mocne opady deszczu"],
-["very heavy rain", "mocne opady deszczu"],
-["extreme rain", "ulewa"],
-["freezing rain", "marznący deszcz"],
-["light intensity shower rain", "lekki deszcz"],
-["shower rain", "deszcz"],
-["heavy intensity shower rain", "intensywny deszcz"],
-["ragged shower rain", "przejściowe opady deszczu"],
-["Snow", "śnieg"],
-["Heavy snow", "Mocne opady śniegu"],
-["Sleet", "śnieg z deszczem"],
-["Light shower sleet", "śnieg z deszczem"],
-["Shower sleet", "śnieg z deszczem"],
-["Light rain and snow", "śnieg z deszczem"],
-["Rain and snow", "deszcz i śnieg"],
-["Light shower snow", "lekkie opady śniegu"],
-["Shower snow", "lekki śnieg"],
-["Heavy shower snow", "opady śniegu"],
-["Smoke", "smog"],
-["Haze", "mgła"],
-["sand/ dust whirls", "wiry piasku / pyłu"],
-["fog", "mgła"],
-["sand", "piasek"],
-["dust", "kurz"],
-["volcanic ash", "pył wulkaniczny"],
-["squalls", "szkwał"],
-["tornado", "tornado"],
-["scattered clouds", "rozproszone chmury"],
-["broken clouds", "zachmurzenie z przejaśnieniami chmury"],
-["overcast clouds", "duże zachmurzenie"],
-["few clouds", "niewielkie zachmurzenie"],
-["clear sky", "czyste niebo"],
+   ["heavy intensity drizzle rain", "intensywna mżawka"],
+   ["shower rain and drizzle", "deszcz"],
+   ["heavy shower rain and drizzle", "ulewny deszcz i mżawka"],
+   ["shower drizzle", "mżawka"],
+   ["drizzle", "mżawka"],
+   ["light rain", "lekki deszcz"],
+   ["moderate rain", "umiarkowane opady"],
+   ["heavy intensity rain", "mocne opady deszczu"],
+   ["very heavy rain", "mocne opady deszczu"],
+   ["extreme rain", "ulewa"],
+   ["freezing rain", "marznący deszcz"],
+   ["light intensity shower rain", "lekki deszcz"],
+   ["shower rain", "deszcz"],
+   ["heavy intensity shower rain", "intensywny deszcz"],
+   ["ragged shower rain", "przejściowe opady deszczu"],
+   ["Snow", "śnieg"],
+   ["Heavy snow", "Mocne opady śniegu"],
+   ["Sleet", "śnieg z deszczem"],
+   ["Light shower sleet", "śnieg z deszczem"],
+   ["Shower sleet", "śnieg z deszczem"],
+   ["Light rain and snow", "śnieg z deszczem"],
+   ["Rain and snow", "deszcz i śnieg"],
+   ["Light shower snow", "lekkie opady śniegu"],
+   ["Shower snow", "lekki śnieg"],
+   ["Heavy shower snow", "opady śniegu"],
+   ["Smoke", "smog"],
+   ["Haze", "mgła"],
+   ["sand/ dust whirls", "wiry piasku / pyłu"],
+   ["fog", "mgła"],
+   ["sand", "piasek"],
+   ["dust", "kurz"],
+   ["volcanic ash", "pył wulkaniczny"],
+   ["squalls", "szkwał"],
+   ["tornado", "tornado"],
+   ["scattered clouds", "rozproszone chmury"],
+   ["broken clouds", "zachmurzenie z przejaśnieniami chmury"],
+   ["overcast clouds", "duże zachmurzenie"],
+   ["few clouds", "niewielkie zachmurzenie"],
+   ["clear sky", "czyste niebo"],
 
 
 ];
 
 export default translation;
-
-// var translation = [
-//    { "en": "light rain", "pl": "lekki deszcz" },
-//    { "en": "few clouds", "pl": "niewielkie zachmurzenie" },
-//    { "en": "scattered clouds", "pl": "rozproszone chmury" },
-//    { "en": "broken clouds", "pl": "przejaśnienia" },
-//    { "en": "shower rain", "pl": "lekki deszcz" },
-//    { "en": "rain", "pl": "deszcz" },
-//    { "en": "thunderstorm", "pl": "burza" },
-
-//    { "en": "thunderstorm with rain", "pl": "burza i deszcz" },
-
-//    { "en": "thunderstorm with heavy rain", "pl": "burza i deszcz" },
-
-//    { "en": "light thunderstorm", "pl": "lekka burza" },
-
-//    { "en": "heavy thunderstorm", "pl": "ciężka burza z piorunami" },
-
-//    { "en": "ragged thunderstorm", "pl": "przejściowe burze" },
-
-//    { "en": "thunderstorm with light drizzle", "pl": "burza z lekką mżawką" },
-
-//    { "en": "thunderstorm with drizzle", "pl": "burza z  mżawką" },
-
-//    { "en": "thunderstorm with heavy drizzle", "pl": "burza z silną mżawką" },
-
-//    { "en": "heavy intensity drizzle", "pl": "intensywna mżawka" },
-
-//    { "en": "light intensity drizzle rain", "pl": "mżawka, deszcz, natężenie światła" },
-
-//    { "en": "drizzle rain", "pl": "deszcz, mżawka" },
-
-//    { "en": "heavy intensity drizzle rain", "pl": "intensywna mżawka" },
-
-//    { "en": "shower rain and drizzle", "pl": "deszcz" },
-
-//    { "en": "heavy shower rain and drizzle", "pl": "ulewny deszcz i mżawka" },
-
-//    { "en": "shower drizzle", "pl": "mżawka" },
-
-
-//    { "en": "drizzle", "pl": "mżawka" },
-
-//    { "en": "light rain", "pl": "lekki deszcz" },
-
-//    { "en": "moderate rain", "pl": "umiarkowane opady" },
-
-//    { "en": "heavy intensity rain", "pl": "mocne opady deszczu" },
-
-//    { "en": "very heavy rain", "pl": "mocne opady deszczu" },
-
-//    { "en": "extreme rain", "pl": "ulewa" },
-
-//    { "en": "freezing rain", "pl": "marznący deszcz" },
-
-//    { "en": "light intensity shower rain", "pl": "lekki deszcz" },
-
-//    { "en": "shower rain", "pl": "deszcz" },
-
-
-//    { "en": "heavy intensity shower rain", "pl": "intensywny deszcz" },
-
-//    { "en": "ragged shower rain", "pl": "przejściowe opady deszczu" },
-
-//    { "en": "Snow", "pl": "śnieg" },
-
-//    { "en": "Heavy snow", "pl": "Mocne opady śniegu" },
-
-//    { "en": "Sleet", "pl": "śnieg z deszczem" },
-
-//    { "en": "Light shower sleet", "pl": "śnieg z deszczem" },
-
-//    { "en": "Shower sleet", "pl": "śnieg z deszczem" },
-
-//    { "en": "Light rain and snow", "pl": "śnieg z deszczem" },
-
-//    { "en": "Rain and snow", "pl": "deszcz i śnieg" },
-
-//    { "en": "Light shower snow", "pl": "lekkie opady śniegu" },
-
-//    { "en": "Shower snow", "pl": "lekki śnieg" },
-
-//    { "en": "Heavy shower snow", "pl": "opady śniegu" },
-
-//    { "en": "Smoke", "pl": "smog" },
-
-//    { "en": "Haze", "pl": "mgła" },
-
-//    { "en": "sand/ dust whirls", "pl": "wiry piasku / pyłu" },
-
-//    { "en": "fog", "pl": "mgła" },
-
-//    { "en": "sand", "pl": "piasek" },
-
-//    { "en": "dust", "pl": "kurz" },
-
-//    { "en": "volcanic ash", "pl": "pył wulkaniczny" },
-
-//    { "en": "squalls", "pl": "szkwał" },
-
-//    { "en": "tornado", "pl": "tornado" },
-
-//    { "en": "scattered clouds", "pl": "rozproszone chmury" },
-
-//    { "en": "broken clouds", "pl": "zachmurzenie z przejaśnieniami chmury" },
-
-//    { "en": "overcast clouds", "pl": "duże zachmurzenie" },
-
-//    { "en": "few clouds", "pl": "niewielkie zachmurzenie" },
-
-//    { "en": "clear sky", "pl": "czyste niebo" }
-// ];
